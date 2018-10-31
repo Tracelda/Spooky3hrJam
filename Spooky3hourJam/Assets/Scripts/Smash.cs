6009fd23d75878dacfa7356a72213ed2ed682182
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2018-10-31 14:55:01 +0000

    Tony, simple score Script

---
+++ Spooky3hourJam/Assets/Scripts/Smash.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Smash : MonoBehaviour
+{
+    private float Score = 0.0f;
+    private int Multiplier = 1;
+    private int Streak = 0;
+
+    void Update()
+    {
+        //click
+        if(Input.GetMouseButtonDown(0))
+        {
+            //increment score times multipler
+            Score += 1 * Multiplier;
+            //increment Streak
+            Streak++;
+            //5 in a row
+            if (Streak == 5)
+            {
+                //increment multiplier
+                Multiplier++;
+                Debug.Log("Multiplier Activated");
+            }
+            Debug.Log(Score);
+            Debug.Log(Streak);
+            Debug.Log(Multiplier);
+        }
+
+    }
+
+
+
+}
