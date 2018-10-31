70fcdd821699cada3b541c6f452e73b73bfc8e54
Jonathan Carter <jmc@devj.uk>
2018-10-31 14:37:27 +0000

    Assets Added & PlayerController Start

---
+++ Spooky3hourJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{	
+	
+	void Update ()
+	{
+
+		RaycastHit Hit;														// Raycasthit setup
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);			// ray to follow the mouse position on the scene
+
+
+		if (Input.GetMouseButton(0))
+		{
+			if (Physics.Raycast(ray, out Hit))
+			{
+				Debug.Log("test2");
+			}
+		}
+	}
+}
