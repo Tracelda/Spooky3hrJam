8faa1f58c150ad0be2bdf79a3a8cc1a1aa132f04
Olly Mills <kinglslbob@gmail.com>
2018-10-31 14:19:25 +0000

    pumpkinSpawning

---
+++ Spooky3hourJam/Assets/Scripts/PumpkinBlopper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinBlopper : MonoBehaviour {
+
+    public GameObject pumpkin;
+    public bool isActive;
+
+    public float upTimers;
+    private float curTimer;
+	// Use this for initialization
+	void Start () {
+        pumpkin = transform.GetChild(0).gameObject;
+        pumpkin.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(isActive==true)
+        {
+
+            curTimer += Time.deltaTime;
+            if(curTimer>upTimers)
+            {
+                curTimer = 0;
+                deActivate();
+            }
+        }
+	}
+
+    public void activate()
+    {
+        isActive = true;
+        pumpkin.SetActive(true);
+    }
+
+    public void deActivate()
+    {
+        isActive = false;
+        pumpkin.SetActive(false);
+    }
+
+
+}
