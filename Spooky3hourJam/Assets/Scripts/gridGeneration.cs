945dc373f5aa431523e630b25c0a262c1f01dcab
Olly Mills <kinglslbob@gmail.com>
2018-10-31 14:41:07 +0000

    basic grid

--- Spooky3hourJam/Assets/Scripts/gridGeneration.cs
+++ Spooky3hourJam/Assets/Scripts/gridGeneration.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class gridGeneration : MonoBehaviour {
 
+    public GameObject[,] pumpKingBoard;
+    public GameObject pumpkingSpawner;
+    public int gridDimentions;
 	// Use this for initialization
 	void Start () {
-		
+        setupBoard(gridDimentions);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    public void setupBoard(int gridDimentions)
+    {
+        pumpKingBoard = new GameObject[gridDimentions, gridDimentions];
+        for (int x = 0; x < gridDimentions; x++)
+        {
+            for (int y = 0; y < gridDimentions; y++)
+            {
+                pumpKingBoard[x, y] = Instantiate(pumpkingSpawner, new Vector3(x, 0, y), Quaternion.identity) as GameObject;
+                pumpKingBoard[x, y].transform.parent = transform;
+            }
+        }
+    }
 }
